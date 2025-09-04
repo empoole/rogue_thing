7c17c717c753b4319918543ca8b9eeac0d4e6969
Eric Poole <admin.poole@throtle.io>
2025-09-04 15:34:58 -0400

    Refactors

--- game_map.py
+++ game_map.py
@@ -27,6 +27,10 @@ class GameMap:
 		)
 
 	@property
+	def gamemap(self) -> GameMap:
+		return self
+
+	@property
 	def actors(self) -> Iterator[Actor]:
 		"""Iterate over this map's living actors."""
 		yield from (
