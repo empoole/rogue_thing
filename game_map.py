c9778a1b10c23e466e58443e0cddac2fb626637c
Eric Poole <admin.poole@throtle.io>
2025-09-04 10:32:37 -0400

    Combat etc

--- game_map.py
+++ game_map.py
@@ -29,7 +29,7 @@ class GameMap:
 	@property
 	def actors(self) -> Iterator[Actor]:
 		"""Iterate over this map's living actors."""
-		yeild from (
+		yield from (
 			entity
 			for entity in self.entities
 			if isinstance(entity, Actor) and entity.is_alive
@@ -73,6 +73,12 @@ class GameMap:
 			default=tile_types.SHROUD,
 		)
 
-		for entity in self.entities:
+		entities_sorted_for_rendering = sorted(
+			self.entities, key=lambda x: x.render_order.value
+		)
+
+		for entity in entities_sorted_for_rendering:
 			if self.visible[entity.x, entity.y]:
-				console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
+				console.print(
+					x=entity.x, y=entity.y, string=entity.char, fg=entity.color
+				)
